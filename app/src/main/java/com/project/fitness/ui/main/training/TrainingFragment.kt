c2350bc2fe86f7bae91728caa51cc8c12827d773
NurMuhammadSyaifuddin <nurm9301@gmail.com>
2021-12-02 22:11:32 +0700

    Add easy permission to training fragment

--- app/src/main/java/com/project/fitness/ui/main/training/TrainingFragment.kt
+++ app/src/main/java/com/project/fitness/ui/main/training/TrainingFragment.kt
@@ -1,13 +1,18 @@
 package com.project.fitness.ui.main.training
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.project.fitness.databinding.FragmentTrainingBinding
+import com.project.fitness.utils.hasLocPermission
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
-class TrainingFragment : Fragment() {
+class TrainingFragment : Fragment(), EasyPermissions.PermissionCallbacks {
 
     private var _binding: FragmentTrainingBinding? = null
     private val binding get() = _binding as FragmentTrainingBinding
@@ -26,4 +31,69 @@ class TrainingFragment : Fragment() {
         _binding = null
     }
 
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {}
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)){
+            AppSettingsDialog.Builder(this).build().show()
+        }else{
+            reqPermission()
+        }
+    }
+
+    private fun reqPermission() {
+        if (hasLocPermission(requireContext())){
+            return
+        }
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q){
+            EasyPermissions.requestPermissions(
+                this,
+                "We need your location permissions to continue",
+                REQ_CODE_LOCATION_PERMISSIONS,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        }else{
+            EasyPermissions.requestPermissions(
+                this,
+                "We need your location permissions to continue",
+                REQ_CODE_LOCATION_PERMISSIONS,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+                Manifest.permission.ACTIVITY_RECOGNITION
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    companion object {
+
+        private const val PARAM1 = "param1"
+        private const val PARAM2 = "param2"
+        private const val REQ_CODE_LOCATION_PERMISSIONS = 0
+
+        @Volatile
+        private var instance: TrainingFragment? = null
+
+        fun getInstance(param1: String, param2: String): TrainingFragment =
+            instance ?: synchronized(this) {
+                instance ?: TrainingFragment().apply {
+                    arguments = Bundle().apply {
+                        putString(PARAM1, param1)
+                        putString(PARAM2, param2)
+                    }
+                    instance = this
+                }
+            }
+    }
 }
--- app/src/main/java/com/project/fitness/utils/Utils.kt
+++ app/src/main/java/com/project/fitness/utils/Utils.kt
@@ -1,9 +1,13 @@
 package com.project.fitness.utils
 
+import android.content.Context
+import android.os.Build
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.launch
+import pub.devrel.easypermissions.EasyPermissions
 import java.util.concurrent.Executors
+import android.Manifest
 
 const val country = "id"
 const val category = "sport"
@@ -13,3 +17,19 @@ private val dispatcher = Executors.newSingleThreadExecutor().asCoroutineDispatch
 private val scope = CoroutineScope(dispatcher)
 
 fun runWithBackgroundThread(block: () -> Unit) = scope.launch { block }
+
+fun hasLocPermission(context: Context) =
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q){
+        EasyPermissions.hasPermissions(
+            context,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+    }else{
+        EasyPermissions.hasPermissions(
+            context,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_BACKGROUND_LOCATION
+        )
+    }
